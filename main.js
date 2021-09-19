72ef0ae5bd615ae3f8a4acd2cb66053fa3f92e95
ynnarg <86639904+ynnarg@users.noreply.github.com>
2021-09-19 10:33:42 +1000

    Update main.js

--- main.js
+++ main.js
@@ -5,7 +5,7 @@ class Complex {
         this.imag = imag;
         this.magnitude = Math.sqrt(real*real + imag*imag);
     }
-    
+
     get magnitude() {return this.magnitude;}
 
     mul(c) {
@@ -31,6 +31,17 @@ let dY = Math.abs(lY - rY);
 let iX = dX / screenSize.X;
 let iY = dY / screenSize.Y;
 
+let colors = [
+    [255, 0, 0],
+    [255, 255, 0]
+]
+
+function lerp(a, b, x) {
+    return a + (b - a) * x
+}
+
+
+ctx.lineWidth = "1";
 for( let x = lX; x <= rX; x += iX ) {
     for( let y = lY; y <= rY; y += iY ) {
         let c = new Complex(x, y);
@@ -38,6 +49,19 @@ for( let x = lX; x <= rX; x += iX ) {
         let i = 0;
         while( c.magnitude < 2 && i < iters ) {
             c = c.mul(c);
+            i += 1;
         }
+        i = colors.length / i;
+        let lower = Math.floor(i);
+        let higher = Math.ceil(i);
+        if (lower < 0) {lower = colors.length - 1;}
+        if (higher >= colors.length) {higher = 0;}
+        let diff = i - lower;
+
+        let col = [lerp(lower[1], higher[1], diff), lerp(lower[2], higher[2], diff), lerp(lower[3], higher[3], diff)];
+        ctx.beginPath();
+        ctx.strokeStyle = "rgb(" + toString(col[1]) + ", " + toString(col[2]) + ", " + toString(col[3]) + ")";
+        ctx.rect((x + dX / 2) / 2 * screenSize.X, (y + dY / 2) / 2 * screenSize.Y , 1, 1);
+        ctx.stroke();
     }
 }
